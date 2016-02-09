f4a2e088f644be74f241194b938c9f18b7a3641c
Kevin Hoffman <kehoffman@pivotal.io>
2016-02-09 07:35:02 -0500

    First test pass.

--- handlers.go
+++ handlers.go
@@ -8,15 +8,18 @@ import (
 
 func addTelemetryHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusCreated, "tbd")
 	}
 }
 
 func addAlertHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusCreated, "tbd")
 	}
 }
 
 func addPositionHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusCreated, "tbd")
 	}
 }
