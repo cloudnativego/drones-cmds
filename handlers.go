84ed306636f9c9d804474a36406601bc571c1f0e
Kevin Hoffman <kehoffman@pivotal.io>
2016-02-10 10:02:12 -0500

    placeholders

--- handlers.go
+++ handlers.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	dronescommon "github.com/cloudnativego/drones-common"
 	"github.com/unrolled/render"
 )
 
 func addTelemetryHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		formatter.JSON(w, http.StatusCreated, "tbd")
+		evt := dronescommon.TelemetryUpdatedEvent{
+			DroneID:          "foo",
+			RemainingBattery: 1,
+			Uptime:           1,
+			CoreTemp:         1,
+			ReceivedOn:       time.Now().UnixNano(),
+		}
+		formatter.JSON(w, http.StatusCreated, evt)
 	}
 }
 
