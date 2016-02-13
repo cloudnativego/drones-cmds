716df26e0769941302f9a7d8a1af9b4ecc506181
Kevin Hoffman <kehoffman@pivotal.io>
2016-02-13 09:13:48 -0500

    Prepping for wercker

--- amqp_dispatcher.go
+++ amqp_dispatcher.go
@@ -6,6 +6,8 @@ type amqpDispatcher struct {
 	tbd string
 }
 
+// TODO - TDD functionality for this
+
 // DispatchMessage implementation of dispatch message interface method
 func (q *amqpDispatcher) DispatchMessage(queue string, message interface{}) (err error) {
 	_, err = json.Marshal(message)
