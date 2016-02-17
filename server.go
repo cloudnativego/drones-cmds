9047a349aa563d5dfbd214164daabf48241d40ba
Kevin Hoffman <kehoffman@pivotal.io>
2016-02-17 06:40:32 -0500

    Added a few minor tests to prepare server for starting with a real Rabbit backing service #codeforaubrey

--- server.go
+++ server.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/cloudfoundry-community/go-cfenv"
+	"github.com/cloudnativego/cf-tools"
 	"github.com/cloudnativego/drones-cmds/fakes"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
+	"github.com/streadway/amqp"
 	"github.com/unrolled/render"
 )
 
@@ -17,10 +24,21 @@ func NewServer() *negroni.Negroni {
 	n := negroni.Classic()
 	mx := mux.NewRouter()
 
-	positionDispatcher := fakes.NewFakeQueueDispatcher()
-	telemetryDispatcher := fakes.NewFakeQueueDispatcher()
-	alertDispatcher := fakes.NewFakeQueueDispatcher()
-	// TODO - if we detect real bound Rabbit, use the AMQP dispatcher
+	var positionDispatcher queueDispatcher
+	var telemetryDispatcher queueDispatcher
+	var alertDispatcher queueDispatcher
+
+	appEnv, err := cfenv.Current()
+	if err != nil {
+		fmt.Printf("Failed to get a CF environment, %s. Using fake defaults.\n", err)
+		positionDispatcher = fakes.NewFakeQueueDispatcher()
+		telemetryDispatcher = fakes.NewFakeQueueDispatcher()
+		alertDispatcher = fakes.NewFakeQueueDispatcher()
+	} else {
+		positionDispatcher = buildDispatcher("positions", appEnv)
+		telemetryDispatcher = buildDispatcher("telemtry", appEnv)
+		alertDispatcher = buildDispatcher("alerts", appEnv)
+	}
 
 	initRoutes(mx, formatter, telemetryDispatcher, alertDispatcher, positionDispatcher)
 
@@ -32,4 +50,59 @@ func initRoutes(mx *mux.Router, formatter *render.Render, telemetryDispatcher qu
 	mx.HandleFunc("/api/cmds/telemetry", addTelemetryHandler(formatter, telemetryDispatcher)).Methods("POST")
 	mx.HandleFunc("/api/cmds/alerts", addAlertHandler(formatter, alertDispatcher)).Methods("POST")
 	mx.HandleFunc("/api/cmds/positions", addPositionHandler(formatter, positionDispatcher)).Methods("POST")
+}
+
+func resolveAMQPURL(appEnv *cfenv.App) string {
+	url, err := cftools.GetVCAPServiceProperty("rabbit", "uri", appEnv)
+	if err != nil {
+		fmt.Println("Failed to detect bound service for rabbit. Falling back to in-memory dispatcher (fake)")
+		return "fake://foo"
+	}
+	if len(url) < 10 {
+		fmt.Printf("URL detected for bound rabbit service not valid, was '%s'. Falling back to in-memory fake.\n", url)
+		return "fake://foo"
+	}
+	return url
+}
+
+func buildDispatcher(queueName string, appEnv *cfenv.App) queueDispatcher {
+	url := resolveAMQPURL(appEnv)
+	if strings.Compare(url, "fake://foo") == 0 {
+		fmt.Println("Building fake dispatcher")
+		return fakes.NewFakeQueueDispatcher()
+	}
+	return createAMQPDispatcher(queueName, url)
+}
+
+func createAMQPDispatcher(queueName string, url string) queueDispatcher {
+	fmt.Printf("\nUsing URL (%s) for Rabbit.\n", url)
+
+	conn, err := amqp.Dial(url)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		queueName, // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	dispatcher := NewAMQPDispatcher(ch, q.Name, false)
+	return dispatcher
+}
+
+// If we did detect a bound service, failing to connect to it should be a fatal,
+// crash-inducing error.
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
 }
---
+++ server_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/cloudfoundry-community/go-cfenv"
+)
+
+func TestResolvesProperRabbitURL(t *testing.T) {
+	fakeVCAP := []string{
+		`VCAP_APPLICATION={}`,
+		`VCAP_SERVICES={"cloudamqp": [{"credentials": {"uri": "amqp://foo.bar"}, "label": "p-rabbitmq", "name": "rabbit", "syslog_drain_url": "", "tags": []}]}`,
+	}
+
+	testEnv := cfenv.Env(fakeVCAP)
+	cfenv, err := cfenv.New(testEnv)
+
+	if cfenv == nil || err != nil {
+		t.Errorf("cfenv is nil: %v", err.Error())
+	}
+
+	url := resolveAMQPURL(cfenv)
+	if strings.Compare(url, "fake://foo") == 0 {
+		t.Errorf("Got the fake URL when we should've gotten the proper URL.")
+	}
+}
+
+func TestFallsBackToFakeURLWhenNoBoundService(t *testing.T) {
+	fakeVCAP := []string{
+		`VCAP_APPLICATION={}`,
+		`VCAP_SERVICES={}`,
+	}
+
+	testEnv := cfenv.Env(fakeVCAP)
+	cfenv, err := cfenv.New(testEnv)
+
+	if cfenv == nil || err != nil {
+		t.Errorf("cfenv is nil: %v", err.Error())
+	}
+
+	url := resolveAMQPURL(cfenv)
+	if strings.Compare(url, "fake://foo") != 0 {
+		t.Errorf("Should have gotten the fake url, but didn't, got %s instead.", url)
+	}
+}
